fb2aea7aca1cd814546006f16e08d2dfa46cb34b
Zagir <zagir7735@gmail.com>
2023-05-08 20:57:34 +0500

    Сделал стартовую страницу, написал модели

---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, help_text="Введите жанр книги")
+
+    def str(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=2550, help_text="Введите краткое описание книги")
+    genre = models.ManyToManyField(Genre, help_text="Выберите жанр для этой книги")
+    def str(self):
+        return self.title
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Мертв', null=True, blank=True)
+    def str(self):
+        return '%s, %s' % (self.last_name, self.first_name)
